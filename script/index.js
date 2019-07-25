6d4c7759a0c92d2fdbac7ea3433cf0b7da2c8748
josekb <kewinbarboza@gmail.com>
2019-07-24 22:25:57 -0400

    efectos

--- script/index.js
+++ script/index.js
@@ -26,12 +26,36 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 window.onscroll = function() {
     var element = document.getElementById("navbar-sombra");
-
-    if(window.scrollY > '700'){
-        console.log('600');
-        element.classList.add("menuSombra");
-        element.classList.add("fixed-top");
+    const acerca =  document.getElementById('acerca');
+    const experiencia =  document.getElementById('experiencia');
+    const portafolio =  document.getElementById('portafolio');
+    const herramientas =  document.getElementById('herramientas');
+    const contacto =  document.getElementById('contacto');
+    
+    if(window.scrollY > '1'){
+        element.classList.add("menuSombra","fixed-top",'animated', 'pulse');
+        acerca.classList.add('animated', 'fadeIn','delay-1s')
     }else if(window.scrollY < '700'){
         element.classList.remove("menuSombra"); 
     }
+
+    if(window.scrollY > '150'){
+        experiencia.classList.add('animated', 'fadeIn','delay-2s')
+    }
+
+    if(window.scrollY > '950'){
+        portafolio.classList.add('animated', 'fadeIn','delay-2s')
+        
+    }
+    if(window.scrollY > '2100'){
+        herramientas.classList.add('animated', 'fadeIn','delay-2s')
+        
+    }
+
+    if(window.scrollY > '3000'){
+        contacto.classList.add('animated', 'fadeIn','delay-2s')
+        
+    }
 };
+
+
