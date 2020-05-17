15aff8cba7152471d3ef5e4898040f365ade0df0
kewinbarboza <kewinbarboza@gmail.com>
2020-05-17 17:22:49 -0400

    image link de portafolio

--- script/github.js
+++ script/github.js
@@ -13,9 +13,9 @@ function cardRepos(data){
   data.forEach((repos, nameImage) => {
     contentRepos.innerHTML += `
       <div class="col-3 mt-5">
-        <div class="card shadow border-0 h-100">
+        <div class="card shadow border-0 h-100 p-3">
           <img src="./imagenes/${nameImage++}.jpg" class="card-img-top" height="150" width="100" alt="${repos.name}">
-          <div class="card-body px-2">
+          <div class="card-body px-0">
             <h6 class="card-title mb-1">${repos.name}</h5>
             <p class="card-text mb-1">${repos.description}</p>
             <p class="card-text"><small class="text-muted"><a href="${repos.homepage}" target="_blanck">Ver mas</a></small></p>
